3294516ef0b973385176b7d457848e58886f7624
Martin Kurowski <martin.kurowski@gmail.com>
2017-02-21 14:16:22 -0600

    Add files via upload

--- src/app/songs/songs.component.ts
+++ src/app/songs/songs.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 import { SpotifyService } from '../service/spotify.service';
+import { Track } from '../model/track';
 import { Album } from '../model/album';
+
+
 
 @Component({
   moduleId: module.id,
@@ -8,20 +11,44 @@ import { Album } from '../model/album';
   templateUrl: './songs.component.html',
   styleUrls: ['./songs.component.css']
 })
-export class SongsComponent implements OnInit {
-  albums: Album[];
+export class SongsComponent implements OnChanges {
+  
+  tracks: any[] = [];
+  
   @Input()
   selectedAlbum: Album;
+  
+  selectedTrack: Track;
 
   constructor(private spotifyService: SpotifyService, ) { }
 
-  ngOnInit() {
-    this.spotifyService.getAlbums().then(albums => this.albums = albums);
+  ngOnChanges() {
+
+    console.log('Find tracks - onChange');
+
+   if (this.selectedAlbum) {
+     console.log('Find tracks for '+this.selectedAlbum.id);
+     this.spotifyService.getAlbumTracks(this.selectedAlbum.id).then(track => this.tracks = track);
+
+    // for (var item of this.tracks) {
+      //    console.log(item.name +' | '+ item.id ); 
+      // } 
+   }
+  
 
   }
 
-  onSelect(album: Album): void {
-    console.log(album.id);
-  }
+  onSelect(track: Track): void {
+    this.selectedTrack = track;
+    console.log('Selected track id: '+track.id+' -- '+track.preview_url);
 
+    let audio = new Audio();
+    audio.src = track.preview_url;
+    audio.load();
+    audio.play();
+
+ //   const newAudioObject = new Audio(track.preview_url);
+ //   newAudioObject.play();
+
+  }
 }
