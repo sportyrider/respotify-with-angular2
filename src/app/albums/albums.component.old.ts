6879a42baf2fffaca06a7257ce68bf4ad56c8700
Martin Kurowski <martin.kurowski@gmail.com>
2017-02-21 14:18:04 -0600

    Add files via upload

--- src/app/albums/albums.component.old.ts
+++ src/app/albums/albums.component.old.ts
@@ -12,10 +12,17 @@ import { SAlbum } from '../model/salbum';
   styleUrls: ['./albums.component.css']
 })
 export class AlbumsComponent implements OnInit {
+  album: Album;
+//  albums: Album[];
 
+  id: string;
   private sub: any;
-
   selectedAlbum: Album;
+
+  salbums: SAlbum[];
+
+  out: String;
+
   albums: any[] = [];
 
   constructor(
@@ -25,21 +32,37 @@ export class AlbumsComponent implements OnInit {
   ngOnInit(): void {
 
     this.sub = this.route.params.subscribe(params => {
+
+      this.id = params['artist']; // (+) converts string 'id' to a number
+      console.log('found' + this.id);
+    
+      // In a real app: dispatch action to load the details here.
+
+      this.route.params
+        .switchMap((params: Params) => this.spotifyService.getAlbum(+params['artist']))
+        .subscribe(album => this.album = album);
+
+   //   this.spotifyService.getAlbums().then(albums => this.albums = albums);
+    //  this.spotifyService.getAlbumsApi().then(salbums => this.salbums = salbums);
+    //  this.spotifyService.getAlbumsApiString().then(out => this.out = out);
+   //   console.log('API:'+this.salbums);
+   //   console.log('API2:'+this.out);
+
+     //  this.spotifyService.getArtistAlbums().then(album => this.albums = album);
+
       this.route.params
         .switchMap((params: Params) => this.spotifyService.getArtistAlbums(params['artist']))
         .subscribe(album => this.albums = album);
 
   //     for (var item of this.albums) {
   //        console.log(item.album_type +' | '+ item.name + ' | '+ item.images[1].url); 
-  //     }   
-
+  //     }       
     });
 
   }
 
   onSelect(album: Album): void {
     this.selectedAlbum = album;
-    console.log(album.id);
 
   }
 
